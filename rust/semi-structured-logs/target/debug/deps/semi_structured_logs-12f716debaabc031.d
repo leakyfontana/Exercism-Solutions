9c976b4db598b39dac939236746d042576b16a58
Xander Dyer <xander.dyer@hotmail.com>
2021-12-18 17:14:55 -0500

    Commit after first CLI test

---
+++ rust/semi-structured-logs/target/debug/deps/semi_structured_logs-12f716debaabc031.d
@@ -0,0 +1,5 @@
+/Users/xanderdyer/Exercism/rust/semi-structured-logs/target/debug/deps/semi_structured_logs-12f716debaabc031: tests/semi-structured-logs.rs
+
+/Users/xanderdyer/Exercism/rust/semi-structured-logs/target/debug/deps/semi_structured_logs-12f716debaabc031.d: tests/semi-structured-logs.rs
+
+tests/semi-structured-logs.rs:
---
+++ rust/semi-structured-logs/target/debug/deps/semi_structured_logs-f451cdef2899fe1d.d
@@ -0,0 +1,5 @@
+/Users/xanderdyer/Exercism/rust/semi-structured-logs/target/debug/deps/semi_structured_logs-f451cdef2899fe1d: src/lib.rs
+
+/Users/xanderdyer/Exercism/rust/semi-structured-logs/target/debug/deps/semi_structured_logs-f451cdef2899fe1d.d: src/lib.rs
+
+src/lib.rs:
