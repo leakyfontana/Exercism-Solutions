f4cedda59605adf5adefb1ab9675279a55496011
Xander Dyer <xander.dyer@hotmail.com>
2022-01-09 22:05:58 -0500

    finished low-power-embedded-game

--- rust/low-power-embedded-game/src/lib.rs
+++ rust/low-power-embedded-game/src/lib.rs
@@ -6,16 +6,15 @@ pub fn divmod(dividend: i16, divisor: i16) -> (i16, i16) {
     (dividend / divisor, dividend % divisor)
 }
 
-pub fn evens<T>(iter: impl Iterator<Item = T>) -> impl Iterator<Item = T> {
-    unimplemented!("implement `fn evens`");
-    // TODO: remove this; it's only necessary to allow this function to compile
-    // before the student has done any work.
-    std::iter::empty()
+pub fn evens<T>(iter: impl Iterator<Item = T>) -> impl Iterator<Item = T> 
+    //where T: Eq + Copy + std::ops::Rem<Output=T> + TryFrom<u64>
+{
+    iter.step_by(2)    
 }
 
 pub struct Position(pub i16, pub i16);
 impl Position {
     pub fn manhattan(&self) -> i16 {
-        unimplemented!("implement `fn manhattan`")
+        self.0.abs() + self.1.abs()
     }
 }
---
+++ rust/low-power-embedded-game/target/debug/liblow_power_embedded_game.d
@@ -0,0 +1,1 @@
+/Users/xanderdyer/Exercism/rust/low-power-embedded-game/target/debug/liblow_power_embedded_game.rlib: /Users/xanderdyer/Exercism/rust/low-power-embedded-game/src/lib.rs
